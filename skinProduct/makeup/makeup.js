376ad6403ef58da023db7ea53cc59ff8ed43c478
MasiSerhan <maisserhan09@gmail.com>
2024-09-18 06:10:09 +0300

    first commit

--- skinProduct/makeup/makeup.js
+++ skinProduct/makeup/makeup.js
@@ -2,7 +2,7 @@ let sect=[];
 products=localStorage.getItem("checkbox")
 products=JSON.parse(products);
 
-
+console.log(products,products==null)
 if(products==[]||products==""||products==null){
     fetch('../products.json')
     .then(response => response.json())
@@ -11,12 +11,13 @@ if(products==[]||products==""||products==null){
         console.log(products)
     })
 }
- 
+
+ /*
 products.forEach(prod=>{
     if(prod.section=="makeup"){
         sect.push(prod.count);
     }
-})
+})*/
 localStorage.setItem('sections',sect);
 localStorage.setItem('colr',6);
 localStorage.setItem('section',"makeup");
