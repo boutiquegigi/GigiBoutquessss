56809836473da71ea798b150a55bd9ac5e2c7593
MasiSerhan <maisserhan09@gmail.com>
2024-09-18 06:34:43 +0300

    first commit

--- skinProduct/body/body.js
+++ skinProduct/body/body.js
@@ -1,21 +1,30 @@
-var srcs=[];
 let sect=[];
+let products = [];
 
-products=localStorage.getItem("checkbox")
-products=JSON.parse(products);
-if(products==[]||products==""){
-    fetch('../products.json')
+console.log(products,products==null,products==[],products=="")
+if(products==[]||products==""||products==null){
+    fetch('../../products.json')
     .then(response => response.json())
     .then(data => {
         products = data;
+       // localStorage.setItem("checkbox",products)
+        console.log(products)
+        sec()
     })
 }
- 
+/*else{
+   products=localStorage.getItem("checkbox")
+   products=JSON.parse(products); 
+}*/
+function sec(){
+console.log(products)
 products.forEach(prod=>{
     if(prod.section=="body"){
         sect.push(prod.count);
     }
+    console.log(sect)
 })
 localStorage.setItem('sections',sect);
-localStorage.setItem('colr',8);
-localStorage.setItem('section',"body");
+localStorage.setItem('colr',6);
+localStorage.setItem('section',"makeup");
+}
